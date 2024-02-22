e086f9a9e5e7ec977bb73ca84d212c579f07f602
klimenko.ma.1982@gmail.com <klimenko.ma.1982@gmail.com>
2024-02-22 13:36:23 +0300

    Added test routs for REST

--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
 import * as mongoose from 'mongoose';
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 mongoose.connect('mongodb://localhost:27017/knowledgedb')
     .then(() => console.log('DB OK'))
-        .catch((err) => console.log('DB error', err));
+    .catch((err) => console.log('DB error', err));
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3333, (err) => {
-   if (err) {
-       console.log(err);
-       return console.log(err);
-   }
+//test method
+app.get('/', (req, res) => {
+    console.log(req.body);
 
-   console.log('Server up');
+
+    res.json({
+       success: true,
+    });
+});
+
+//test method
+app.post('/auth/login', (req, res) =>{
+
+    const token = jwt.sign({
+        email : req.body.email,
+        fullName: "Bambara Chupara",
+    }, 'secret 123');
+
+    res.json({
+        success: true,
+        token,
+    });
+});
+
+app.listen(3333, (err) => {
+    if (err) {
+        console.log(err);
+        return console.log(err);
+    }
+
+    console.log('Server up');
 });
