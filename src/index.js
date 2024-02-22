493228b47bbb7e775db43bfaf20b59ab0f99aec2
klimenko.ma.1982@gmail.com <klimenko.ma.1982@gmail.com>
2024-02-22 13:36:23 +0300

    Added test routs for REST

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import * as mongoose from 'mongoose';
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import * as UserController from './controllers/UserController.js'
-import {registerValidation, loginValidation} from "./validations.js";
 
 mongoose.connect('mongodb://localhost:27017/knowledgedb')
     .then(() => console.log('DB OK'))
@@ -12,8 +10,29 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, UserController.register);
-app.post('/auth/login', loginValidation, UserController.login);
+//test method
+app.get('/', (req, res) => {
+    console.log(req.body);
+
+
+    res.json({
+       success: true,
+    });
+});
+
+//test method
+app.post('/auth/login', (req, res) =>{
+
+    const token = jwt.sign({
+        email : req.body.email,
+        fullName: "Bambara Chupara",
+    }, 'secret 123');
+
+    res.json({
+        success: true,
+        token,
+    });
+});
 
 app.listen(3333, (err) => {
     if (err) {
