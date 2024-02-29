0745d9231bf4482ccd42d54dd4e814911715b8c0
klimenko.ma.1982@gmail.com <klimenko.ma.1982@gmail.com>
2024-02-29 16:43:37 +0300

    Created some new classes and added login route in UserController.js

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -13,7 +13,7 @@ export const register = async (req, res) => {
 
     try {
         const errors = validationResult(req);
-        if(!errors.isEmpty()){
+        if (!errors.isEmpty()) {
             return res.status(404).json(errors.array());
         }
 
@@ -58,3 +58,49 @@ export const register = async (req, res) => {
     }
 
 };
+
+export const login = async (req, res) => {
+    try {
+        const user = await UserModel.findOne({
+            email: req.body.email,
+        });
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not found'
+            });
+        }
+
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: 'Login or password is invalid'
+            });
+        }
+
+        const token = jwt.sign({
+                _id: user._id
+            },
+            "secret123",
+            {
+                expiresIn: '30d'
+            }
+        );
+
+        const {passwordHash, ...UserData} = user._doc;
+
+        res.json({
+            ...UserData,
+            token
+        });
+
+
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            message: 'Validation is invalid'
+        });
+    }
+}
