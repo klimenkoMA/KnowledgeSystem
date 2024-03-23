2cf3511df077c2e04776324f7b0639c772b354d5
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2024-03-23 17:16:01 +0300

    Added some cosmetic

--- react-intro/src/footer/footer.js
+++ react-intro/src/footer/footer.js
@@ -4,9 +4,9 @@ import { motion } from 'framer-motion';
 const Footer = () => {
     return (
         <motion.footer
-            initial={{ opacity: 3, y: 20 }}
-            animate={{ opacity: 3, y: 0 }}
-            transition={{ duration: 3 }}
+            initial={{ opacity: 1, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 4 }}
         >
             <div className="footer">
                 <p>&copy; 2024 Система знаний</p>
