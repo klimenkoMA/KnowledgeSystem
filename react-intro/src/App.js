518dd301cee4a851437215b79b8a930a6ba77fd6
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2024-03-24 20:08:35 +0300

    Rebuilt css style`s in App.css

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -9,20 +9,23 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
             </header>
-            <body style={{
-                marginTop: `120px`,
-                marginBottom: `100px`,
-                width: `100%`,
-                background: `url(/backFon.jpg`,
-                backgroundSize: `100% 100%`,
-                backgroundRepeat: "no-repeat",
-            }}>
-            <p>
-                <SimpleForm/>
-            </p>
-            <p>
-                <SimpleForm/>
-            </p>
+            <body className="App-body"
+                  style={{
+                      marginTop: `120px`,
+                      marginBottom: `100px`,
+                      width: `100%`,
+                      background: `url(/backFon.jpg`,
+                      backgroundSize: `100% 100%`,
+                      backgroundRepeat: "no-repeat",
+                  }}>
+            <div className="App-main">
+                <p>
+                    <SimpleForm/>
+                </p>
+                <p>
+                    <SimpleForm/>
+                </p>
+            </div>
             </body>
             <footer className="App-footer">
                 <Footer/>
