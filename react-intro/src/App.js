2df21db00d81cf69d0e52276df4456007a6e15ee
klimenko.ma.1982@gmail.com <klimenko.ma.1982@gmail.com>
2024-03-25 16:37:06 +0300

    Rebuilt css styles v2

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -5,19 +5,19 @@ import Footer from "./footer/footer";
 
 function App() {
     return (
-        <div className="App">
+        <div className="App"
+             style={{
+                 width: `100%`,
+                 background: `url(/backFon.jpg`,
+                 backgroundSize: `100% 100%`,
+                 backgroundRepeat: "no-repeat",
+                 height: `max`,
+             }}
+        >
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
             </header>
-            <body className="App-body"
-                  style={{
-                      marginTop: `120px`,
-                      marginBottom: `100px`,
-                      width: `100%`,
-                      background: `url(/backFon.jpg`,
-                      backgroundSize: `100% 100%`,
-                      backgroundRepeat: "no-repeat",
-                  }}>
+            <body className="App-body">
             <div className="App-main">
                 <p>
                     <SimpleForm/>
--- react-intro/src/footer/footer.js
+++ react-intro/src/footer/footer.js
@@ -4,9 +4,9 @@ import { motion } from 'framer-motion';
 const Footer = () => {
     return (
         <motion.footer
-            initial={{ opacity: 1, y: 20 }}
+            initial={{ opacity: 1, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 4 }}
+            transition={{ duration: 3 }}
         >
             <div className="footer">
                 <p>&copy; 2024 Система знаний</p>
